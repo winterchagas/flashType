d686c0bfcad573ab88da0c6b839b75c83822d081
MagicLeo <winterchagas@gmail.com>
2019-09-29 02:30:13 -0700

    main functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,110 @@
-import React from 'react';
+import React, {useState, Fragment} from 'react';
+import {shouldCaptureCharacter} from "./helpers";
+
 import './index.scss';
-import SegundoComponent from './Neno.jsx';
 
-var listaDeNomes = [
-	'NENO',
-	'HARLEY',
-	'MARIA NIDIA',
-];
+const fullSentence = 'abcd';
+const sentenceLength = fullSentence.length;
+const phraseFirstPart = [];
+const phraseErrorPart = [];
+const phraseSecondPart = [...fullSentence];
+const typedPhrase = [];
 
-// [
-// 	<div></div>,
-// 	<div></div>,
-// 	<div></div>,
-// 	<div></div>,
-// 	<div></div>,
-// ]
+function handleCorrectType() {
+  const removedCharacter = phraseSecondPart.shift();
+  phraseFirstPart.push(removedCharacter);
+}
+
+function handleWrongType() {
+  const removedCharacter = phraseSecondPart.shift();
+  phraseErrorPart.push(removedCharacter);
+}
+
+function handleBackspaceType() {
+  if (phraseSecondPart.length < sentenceLength) {
+    if (phraseErrorPart.length > 0) {
+      const removedCharacter = phraseErrorPart.pop();
+      phraseSecondPart.unshift(removedCharacter);
+    } else {
+      const removedCharacter = phraseFirstPart.pop();
+      phraseSecondPart.unshift(removedCharacter);
+    }
+    typedPhrase.pop();
+    return true;
+  }
+  return false;
+}
+
+const hasPreviousError = () => phraseErrorPart.length > 0;
+
+function isEndOfString() {
+  return (
+    phraseFirstPart.length +
+    phraseErrorPart.length
+  ) === sentenceLength;
+}
 
 const App = () => {
-	return (
-		<div className="container">
-			<p>React here!</p>
-			{
-				listaDeNomes.map(function (nome) {
-					return (
-						<div>
-							<div>DIV DE CIMA</div>
-						<SegundoComponent
-							name={nome}
-						/>
-						</div>
-					)
-				})
-			}
-			<p>React here!</p>
-		</div>
-	);
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleTyping = (event) => {
+    if (shouldCaptureCharacter(event.key)) {
+      if (event.key === 'Backspace') {
+        const shouldReRender = handleBackspaceType();
+        if (shouldReRender) {
+          setCurrentIndex(currentIndex - 1);
+        }
+        return;
+      }
+
+      if (isEndOfString(event.key)) {
+        return;
+      }
+
+      if (hasPreviousError()) {
+        handleWrongType();
+      } else {
+        if (event.key === phraseSecondPart[0]) {
+          handleCorrectType();
+        } else {
+          handleWrongType();
+        }
+      }
+      setCurrentIndex(currentIndex + 1);
+      typedPhrase.push(event.key);
+    }
+  };
+
+  return (
+    <div
+      className="phrase-box"
+      tabIndex={0}
+      onKeyDown={handleTyping}
+    >
+      <div className="phrase-box__full">
+        <span>{fullSentence}</span>
+      </div>
+      <div className="phrase-box__break">
+        <span className="phrase-box__first-part">{phraseFirstPart.join('')}</span>
+        <span className="phrase-box__error-part">{phraseErrorPart.join('')}</span>
+        <span className="phrase-box__last-part">{phraseSecondPart.join('')}</span>
+      </div>
+      <div className="phrase-box__type-box">
+        {
+          [...typedPhrase]
+            .map((character, index) => {
+              return <span key={index}>{character}</span>
+            })
+        }
+				{
+					[...phraseSecondPart]
+						.map((character, index) => {
+							return <span key={index}>{character}</span>
+						})
+				}
+      </div>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/helpers.js
@@ -0,0 +1,15 @@
+const allowedCharacters = {
+  q: true, w: true, e: true, r: true, t: true, y: true, u: true,
+  i: true, o: true, p: true, a: true, s: true, d: true, f: true,
+  g: true, h: true, j: true, k: true, l: true, "'": true,
+  Enter: true, z: true, x: true, c: true, v: true, b: true,
+  n: true, m: true, ',': true, '.': true, '?': true, Q: true,
+  W: true, E: true, R: true, T: true, Y: true, U: true, I: true,
+  O: true, P: true, A: true, S: true, D: true, F: true, G: true,
+  H: true, J: true, K: true, L: true, Z: true, X: true, C: true,
+  V: true, B: true, N: true, M: true, ' ': true, 'Backspace': true
+};
+
+export function shouldCaptureCharacter(character) {
+  return allowedCharacters[character];
+}
