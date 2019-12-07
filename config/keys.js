86fbc79a18673376115f2a4519f03a219ba141b5
MagicLeo <winterchagas@gmail.com>
2019-12-07 11:33:11 -0800

    keys for deployment

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+module.exports = {
+  googleClientID: process.env.googleClientID,
+  googleClientSecret: process.env.googleClientSecret,
+  firebaseConfig: {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
+  }
+};
